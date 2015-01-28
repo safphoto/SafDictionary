c23aef43ef19c8357d7d7efdfe9300538b4217da
Scott Flaherty <Scott.Flaherty@mckesson.com>
2015-01-28 13:46:27 -0500

    Refactoring unit tests

--- tests/spec/DictionarySpec.js
+++ tests/spec/DictionarySpec.js
@@ -1,11 +1,25 @@
 describe("Dictionary", function() {
+  var d;
+
+  beforeEach(function() {
+    d = new SAF.Dictionary();
+    d.add("Scott", 49);
+    d.add("Corey", 45);
+    d.add("Cindy", 43);
+    d.add("Shawn", 53);
+    d.add("Steve", 51);
+    d.add("Willie", 57);
+  });
+
+  afterEach(function() {
+    d.clear();
+    d = null;
+  });
 
   describe("When an item is added to the dictionary", function() {
-    var d = new SAF.Dictionary();
-
     it("should have an incremented count", function() {
       var origCount = d.count();
-      d.add('key1', 'value1');
+      d.add('Howard', 49);
       var newCount = d.count();
 
       expect(newCount).toBeGreaterThan(origCount);
@@ -13,12 +27,9 @@ describe("Dictionary", function() {
   });
 
   describe("When an item is removed from the dictionary", function() {
-    var d = new SAF.Dictionary();
-
     it("should have a decremented count", function() {
-      d.add('key1', 'value1');
       var origCount = d.count();
-      d.remove('key1');
+      d.remove('Scott');
       var newCount = d.count();
 
       expect(newCount).toBeLessThan(origCount);
@@ -27,38 +38,20 @@ describe("Dictionary", function() {
 
   describe("When clearing a dictionary", function() {
     var d = new SAF.Dictionary();
-    var newCount;
 
-    beforeAll(function() {
-      for(var i = 0; i < 10; i++) {
-        d.add('key' + i, 'value' + i);
-      }
-
-      d.clear();
-      newCount = d.count();
-    });
+    d.clear();
+    var newCount = d.count();
 
     it("should result in a count of 0", function() {
       expect(newCount).toEqual(0);
     });
 
     it("should not result in a null object ref", function() {
-      expect(d).not.toBeNull(0);
+      expect(d).not.toBeNull();
     });
   });
 
   describe("When getting a list of keys", function() {
-    var d = new SAF.Dictionary();
-
-    beforeAll(function() {
-      d.add("Scott", 49);
-      d.add("Corey", 45);
-      d.add("Cindy", 43);
-      d.add("Shawn", 53);
-      d.add("Steve", 51);
-      d.add("Willie", 57);
-    });
-
     it("should be sorted when specified", function() {
       var keys = d.keys(d.sortOptions.descending);
       expect(keys.join(',')).toEqual('Willie,Steve,Shawn,Scott,Corey,Cindy');
@@ -71,17 +64,6 @@ describe("Dictionary", function() {
   });
 
   describe("When serializing the dictionary", function() {
-    var d = new SAF.Dictionary();
-
-    beforeAll(function() {
-      d.add("Scott", 49);
-      d.add("Corey", 45);
-      d.add("Cindy", 43);
-      d.add("Shawn", 53);
-      d.add("Steve", 51);
-      d.add("Willie", 57);
-    });
-
     it("should output valid JSON", function() {
       var keys = d.keys(d.sortOptions.none);
       expect(d.toString()).toEqual('{"Scott":49,"Corey":45,"Cindy":43,"Shawn":53,"Steve":51,"Willie":57}');
@@ -89,17 +71,6 @@ describe("Dictionary", function() {
   });
 
   describe("When checking to se if the dictionary contains a specific key", function() {
-    var d = new SAF.Dictionary();
-
-    beforeAll(function() {
-      d.add("Scott", 49);
-      d.add("Corey", 45);
-      d.add("Cindy", 43);
-      d.add("Shawn", 53);
-      d.add("Steve", 51);
-      d.add("Willie", 57);
-    });
-
     it("should return 'true' if found", function() {
       var result = d.contains('Scott');
       expect(result).toBeTruthy();
@@ -112,17 +83,6 @@ describe("Dictionary", function() {
   });
 
   describe("When checking to se if the dictionary contains a specific key", function() {
-    var d = new SAF.Dictionary();
-
-    beforeAll(function() {
-      d.add("Scott", 49);
-      d.add("Corey", 45);
-      d.add("Cindy", 43);
-      d.add("Shawn", 53);
-      d.add("Steve", 51);
-      d.add("Willie", 57);
-    });
-
     it("should return the value for the key item if found", function() {
       var result = d.find('Scott');
       expect(result).toEqual(49);
